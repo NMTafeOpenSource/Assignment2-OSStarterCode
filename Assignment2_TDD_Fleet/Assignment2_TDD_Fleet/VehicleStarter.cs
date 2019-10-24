36fcea885d95d733ddccf3ae10234d2971bc95d4
LanangW <lanangwisnugiri@gmail.com>
2019-10-24 10:52:31 +0800

    Added new Classes

---
+++ Assignment2_TDD_Fleet/Assignment2_TDD_Fleet/VehicleStarter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_TDD_Fleet
+{
+    public class VehicleStarter
+    {
+
+        /** Main entry point to the program
+         * @param args the command line arguments
+         */
+        public static void main(String[] args)
+        {
+            Vehicle v = new Vehicle("Ford", "T812", 2014);
+
+            // Vehicle sample distance
+            v.addFuel(new Random().NextDouble() * 10, 1.3);
+
+            v.printDetails();
+            Console.WriteLine("\n\n");
+        }
+
+    }
+}
